9fdfdcf5b854056ac52d2fd1de15005b2b2414e4
Harsha125-art <harshavardhani.d24@iiits.in>
2025-11-03 09:18:59 +0530

    Delete Hackathon-backend directory

--- Hackathon-backend/utils/preprocessing.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def clean_text(text):
-    # Remove emojis
-    text = re.sub(r'[^\x00-\x7F]+','', text)
-    # Remove extra spaces and punctuation
-    text = re.sub(r'[^\w\s]', '', text)
-    text = text.strip().lower()
-    return text
